c58a74957e7aadc5c0b886596dc2f191e11e5573
elodimarci <marton.elodi@vernhelps.com>
2020-11-21 23:17:52 +0100

    fix: clean up and optimise

--- src/util/game-round.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from '../types/game';
-
-export const getCurrentRound = (game: Game) => {
-  if (!game || !game.rounds || game.round === null || game.round === undefined) {
-    return null;
-  }
-
-  return game.rounds[game.round];
-};
