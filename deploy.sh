9c0499ab0edd75385f0f5f2843aecfdf0355d17d
Gergely Bihary <gergely.bihary@prezi.com>
2020-11-22 01:23:07 +0100

    feat: Deployment improvements

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PUBLIC_URL="https://storage.googleapis.com/hacktivity-296321.appspot.com" npm run build
+REACT_APP_ENDPOINT="https://hacktivity-296321.ew.r.appspot.com/" PUBLIC_URL="https://storage.googleapis.com/hacktivity-296321.appspot.com" npm run build
 cd build
 gsutil rm gs://hacktivity-296321.appspot.com/**  
 gsutil -h "Cache-Control:no-cache,max-age=0" cp -r . gs://hacktivity-296321.appspot.com
